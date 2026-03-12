9c618a7121539904d0995091762c63e2af076681
Lucas José <lucas.soares01087986@sesisenaipr.org.br>
2026-03-12 10:04:51 -0300

    aula dia 12/03

---
+++ filmes.py
@@ -0,0 +1,2 @@
+filmes = ["O espetacular Homem aranha", "Vingadores: Guerra Infinita", "Vingadores: Ultimato", "Homem aranha: sem volta pra casa", "Captão América: Guerra Cívil"]
+print(filmes[0], filmes[4])
