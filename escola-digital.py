16dd7bd51ba39e2e03318c7b25f28f1be3913a3c
lucas <lucas.soares01087986@sesisenaipr.org.br>
2026-05-21 10:08:09 -0300

    21/05

--- escola-digital.py
+++ escola-digital.py
@@ -1,23 +1,27 @@
 import json
+def criar_arquivo():
+   with open('alunos.json', 'w')
+def menu():
+    while True:
+      print("1.Cadastrar | 2.Listar | 3.Editar | 4.Excluir | 5.Sair")
+      opçao = input("Opçao: ")
 
-arquivo_nome = "alunos.json"
+    cpf: input("CPF: ")
+    nome: input("Nome: ")
+    telefone: input ("Telefone: ")
+    turma: input("Turma:")
+    idade: input("Idade:")
+
+def cadastro():
+    cadastro = {
+        "cpf": cpf,
+        "nome": nome,
+        "telefone": telefone,
+        "turma": turma,
+        "idade": idade,
+    }
+        
 
 while True:
-    print("\n1.Cadastrar | 2.Listar | 3.Editar | 4.Excluir | 5.Sair")
-    opçao = input("Opçao: ")
-
-    if opçao == "1":
-        cpf = input("CPF: ")
-        if (aluno["cpf"] == cpf for aluno in lista):
-            print("Erro! CPF já existe")
-        else:
-            cadastro = {
-                "cpf": cpf,
-                "nome": input("Nome: "),
-                "telefone": input ("Telefone: "),
-                "turma": input("Turma:"),
-                "idade": input("Idade:"),
-            }
-            lista.append(cadastro)
-            json.dump(lista, open(arquivo_nome, 'w'))
-            print("Salvo")
+      print("1.Cadastrar | 2.Listar | 3.Editar | 4.Excluir | 5.Sair")
+      opçao = input("Opçao: ")
