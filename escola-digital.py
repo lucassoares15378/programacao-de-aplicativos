2944c126309124e0fb22882af38c5f7fee5d621e
lucas <lucas.soares01087986@sesisenaipr.org.br>
2026-05-21 09:28:54 -0300

    21/05

--- escola-digital.py
+++ escola-digital.py
@@ -1,7 +1,23 @@
 import json
-def criar():
-    arquivo = "alunos.json"
+
+arquivo_nome = "alunos.json"
 
 while True:
-    print("1-cadastrar | 2-listar | 3-editar | 4-excluir | 5-sair ")
-    opcao = input("Opção: ")
+    print("\n1.Cadastrar | 2.Listar | 3.Editar | 4.Excluir | 5.Sair")
+    opçao = input("Opçao: ")
+
+    if opçao == "1":
+        cpf = input("CPF: ")
+        if (aluno["cpf"] == cpf for aluno in lista):
+            print("Erro! CPF já existe")
+        else:
+            cadastro = {
+                "cpf": cpf,
+                "nome": input("Nome: "),
+                "telefone": input ("Telefone: "),
+                "turma": input("Turma:"),
+                "idade": input("Idade:"),
+            }
+            lista.append(cadastro)
+            json.dump(lista, open(arquivo_nome, 'w'))
+            print("Salvo")
