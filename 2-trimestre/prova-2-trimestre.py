d0194de9e71795b64a947d37409b102567f1c005
lucas <lucas.soares01087986@sesisenaipr.org.br>
2026-08-14 07:53:00 -0300

    14/08

--- 2-trimestre/prova-2-trimestre.py
+++ 2-trimestre/prova-2-trimestre.py
@@ -1,76 +1,97 @@
 import sqlite3
+
 try:
     conexao = sqlite3.connect('armazem.db')
     cursor = conexao.cursor()
 except sqlite3.Error as erro:
-    print(f"Erro ao conectar o banco de dados {erro}")
+    print(f"Erro ao conectar o banco de dados: {erro}")
     exit()
 
 def cadastrar_operadores():
     try:
         razao_social = input("Qual a razão social? ")
         inscricao_estadual = input("Qual a inscrição estadual? ")
-    
+
         cursor.execute('''
-            CREATE TABLE IF NOT EXISTS operadores_logisticos (
-                id INTEGER PRIMARY AUTOINCREMENT,
-                razao_social TEXT NOT NULL,
-                inscricao_estadual INTEGER
-            )
-            ''')
+        CREATE TABLE IF NOT EXISTS operadores_logisticos(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            razao_social TEXT NOT NULL,
+            inscricao_estadual INTEGER
+        )
+        ''')
+
         comando_inserir = f'''
         INSERT INTO operadores_logisticos(razao_social, inscricao_estadual)
-        VALUES('{razao_social}', '{inscricao_estadual}')'''
+        VALUES('{razao_social}', '{inscricao_estadual}')
+        '''
 
         cursor.execute(comando_inserir)
         conexao.commit()
         print("Cadastro feito com sucesso!")
 
     except sqlite3.Error as erro:
-        print(f"Não foi possível conectar o banco de dados: {erro}.")
+        print(f"Erro no banco de dados ao cadastrar: {erro}")
+
 
 def listar_operadores():
     try:
-        cursor.execute('SELECT FROM * operadores_logisticos')
+        cursor.execute("SELECT * FROM operadores_logisticos")
         linhas = cursor.fetchall()
         if not linhas:
             print("Nenhum cadastro realizado!")
         else:
             for linha in linhas:
-                print(f'''ID {linha[0]} | Razão Social {linha[1]} | Inscrição Estadual {linha[2]}''')
-            print("\n")
+                print(f"ID: {linha[0]} | Razão Social: {linha[1]} | Inscrição Estadual: {linha[2]}")
+        print("\n")
+
     except sqlite3.Error as erro:
-        print(f"Erro ao conectar banco de dados: {erro}.")
+        print(f"Erro ao listar operadores: {erro}")
+
 
 def atualizar_operadores():
     try:
         id_operador = int(input("Qual o ID para realizar a atualização? "))
-        cursor.execute(f"SELECT FROM * operadores_logisticos WHERE id = {id_operador}")
-        operadores_logisticos = cursor.fetchall()
+
+        cursor.execute(f'''SELECT * FROM operadores_logisticos WHERE id = {id_operador}''')
+        operadores_logisticos = cursor.fetchone()
 
         if operadores_logisticos is None:
             print("ID não encontrado!")
         else:
             nova_razao_social = input("Digite a nova razão social: ")
             nova_inscricao_estadual = input("Digite a nova inscrição estadual: ")
-        
-            comando = f''' UPDATE operadores_logisticos SET = razao social = '{nova_razao_social}',inscricao estadual = '{nova_inscricao_estadual}' WHERE id = {id_operador}'''
+
+            comando = f'''
+            UPDATE operadores_logisticos 
+            SET razao_social = '{nova_razao_social}',
+            inscricao_estadual = '{nova_inscricao_estadual}'
+            WHERE id = {id_operador}
+            '''
+
             cursor.execute(comando)
             conexao.commit()
-            print("Dados atualizados")
-    except sqlite3.Error as erro:
-        print(f"Erro ao conectar banco de dados {erro}")
+            print("Dados atualizados com sucesso!")
+
     except ValueError:
-        print("Valor inválido!")
+        print("Valor inválido")
+
     except TypeError:
-        print("Tipo de dao inválido!")
-    except FileNotFoundError as erro:
+        print("Tipo de dado inválido")
+
+    except ZeroDivisionError:
+        print("Divisão por zero")
+
+    except FileNotFoundError:
+        print("Arquivo não encontrado")
+
+    except Exception as erro:
         print(f"Ocorreu um erro: {erro}")
-        
+
 def excluir_operadores():
     try:
         id_operador = int(input("Qual o ID para realizar a exclusão? "))
-        cursor.execute(f"SELECT FROM * operadores_logisticos WHERE id = {id_operador}")
+
+        cursor.execute(f"SELECT * FROM operadores_logisticos WHERE id = {id_operador}")
         operador_existe = cursor.fetchone()
 
         if operador_existe is None:
@@ -82,106 +103,139 @@ def excluir_operadores():
     except ValueError:
         print("O ID precisa ser um número inteiro!")
     except sqlite3.Error as erro:
-        print(f"Erro ao cadastrar banco de dados!")
+        print(f"Erro ao excluir registro: {erro}")
+
 
 def cadastrar_galpão():
     try:
         identificacao_bloco = input("Qual a identificação do bloco? ")
-        id_operador = input("Qual o ID do operador? ")
-    
+        id_operador = int(input("Qual o ID do operador? "))
+
         cursor.execute('''
-            CREATE TABLE IF NOT EXISTS galpoes (
-                id INTEGER PRIMARY AUTOINCREMENT,
-                identificacao_bloco TEXT NOT NULL,
-                id_operador INTEGER,
-                FOREIGN KEY(id_operador) REFERENCES operadores_logisticos(id)       
-            )
-            ''')
+        CREATE TABLE IF NOT EXISTS galpoes(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            identificacao_bloco TEXT NOT NULL,
+            id_operador INTEGER,
+            FOREIGN KEY(id_operador) REFERENCES operadores_logisticos(id)
+        )
+        ''')
+
         cursor.execute(f"SELECT * FROM operadores_logisticos WHERE id = {id_operador}")
         operador = cursor.fetchone()
 
         if operador is None:
             print("Nenhum operador com esse ID cadastrado!")
         else:
-            comando_inserir = f''' INSERT INTO galpoes (identificacao_bloco, id_operador)
-            VALUES ('{identificacao_bloco}', '{id_operador}')
+            comando_inserir = f'''
+            INSERT INTO galpoes(identificacao_bloco, id_operador)
+            VALUES('{identificacao_bloco}', '{id_operador}')
             '''
             cursor.execute(comando_inserir)
             conexao.commit()
             print("Galpão cadastrado!")
-    except sqlite3 as erro:
-        print("Erro ao conectar o banco de dados!")
+
     except ValueError:
         print("O ID precisa ser um número inteiro!")
+    except sqlite3.Error as erro:
+        print(f"Erro no banco de dados ao cadastrar: {erro}")
 
 def listar_galpoes():
     try:
-        cursor.execute(''''SELECT FROM * galpoes''')
+        cursor.execute("SELECT * FROM galpoes")
         linhas = cursor.fetchall()
         if not linhas:
             print("Nenhum cadastro realizado!")
         else:
             for linha in linhas:
-                print(f'''ID {linha[0]} | Identificação de bloco {linha[1]} | ID Operador {linha[2]}''')
-            print("\n")
+                print(f"ID: {linha[0]} | Identificação do bloco: {linha[1]} | ID Operador: {linha[2]}")
+        print("\n")
+
     except sqlite3.Error as erro:
-        print(f"Erro ao conectar banco de dados: {erro}.")
+        print(f"Erro ao listar galpões: {erro}")
+
 
 def atualizar_galpoes():
     try:
         id_galpao = int(input("Qual o ID para realizar a atualização? "))
-        cursor.execute(f"SELECT FROM * operadores_logisticos WHERE id = {id_galpao}")
-        galpao = cursor.fetchall()
+
+        cursor.execute(f'''SELECT * FROM galpoes WHERE id = {id_galpao}''')
+        galpao = cursor.fetchone()
 
         if galpao is None:
             print("ID não encontrado!")
         else:
             nova_identificacao_bloco = input("Digite a nova identificação do bloco: ")
-            novo_id_operador = input("Digite o novo ID operador: ")
-        
-            comando = f''' UPDATE galpoes SET = identificacao bloco = '{nova_identificacao_bloco}', WHERE id = {id_operador}'''
-            cursor.execute(comando)
-            conexao.commit()
-            print("Dados atualizados")
-    except sqlite3.Error as erro:
-        print(f"Erro ao conectar banco de dados {erro}")
+            novo_id_operador = int(input("Digite o novo ID operador: "))
+
+            cursor.execute(
+                f"SELECT * FROM operadores_logisticos WHERE id = {novo_id_operador}"
+            )
+            operador = cursor.fetchone()
+
+            if operador is None:
+                print("Nenhum operador com esse ID cadastrado!")
+            else:
+                comando = f'''
+                UPDATE galpoes 
+                SET identificacao_bloco = '{nova_identificacao_bloco}',
+                id_operador = {novo_id_operador}
+                WHERE id = {id_galpao}
+                '''
+
+                cursor.execute(comando)
+                conexao.commit()
+                print("Dados atualizados com sucesso!")
+
     except ValueError:
-        print("Valor inválido!")
+        print("Valor inválido")
+
     except TypeError:
-        print("Tipo de dao inválido!")
-    except FileNotFoundError as erro:
+        print("Tipo de dado inválido")
+
+    except ZeroDivisionError:
+        print("Divisão por zero")
+
+    except FileNotFoundError:
+        print("Arquivo não encontrado")
+
+    except Exception as erro:
         print(f"Ocorreu um erro: {erro}")
-        
+
 def excluir_galpoes():
     try:
-        id_operador = int(input("Qual o ID para realizar a exclusão? "))
-        cursor.execute(f"SELECT FROM * operadores_logisticos WHERE id = {id_operador}")
-        operador_existe = cursor.fetchone()
+        id_galpao = int(input("Qual o ID para realizar a exclusão? "))
 
-        if operador_existe is None:
-            print("Nenhum operador encontrado")
+        cursor.execute(f"SELECT * FROM galpoes WHERE id = {id_galpao}")
+        galpao_existe = cursor.fetchone()
+
+        if galpao_existe is None:
+            print("Nenhum galpão encontrado")
         else:
-            cursor.execute(f"DELETE FROM operadores_logisticos WHERE id = {id_operador}")
+            cursor.execute(f"DELETE FROM galpoes WHERE id = {id_galpao}")
             conexao.commit()
-            print("Operador excluído com sucesso!")
+            print("Galpão excluído com sucesso!")
+
     except ValueError:
         print("O ID precisa ser um número inteiro!")
     except sqlite3.Error as erro:
-        print(f"Erro ao cadastrar banco de dados!")
-
-
+        print(f"Erro ao excluir registro: {erro}")
 
 
 def menu():
     while True:
         try:
-            print("===SISTEMA DE CADASTRO LOGÍSTICO===")
-            print("1- CADASTRAR OPERADOR")
-            print("2- LISTAR TUDO CADASTADO")
-            print("3- ATUALIZAR CADASTRO")
-            print("4- DELETAR CADASTRO")
+            print("\n1. Cadastrar Operador")
+            print("2. Listar Operadores")
+            print("3. Alterar Operador")
+            print("4. Excluir Operador")
+            print("5. Cadastrar Galpão")
+            print("6. Listar Galpões")
+            print("7. Alterar Galpão")
+            print("8. Excluir Galpão")
+            print("9. Sair")
 
-            opcao = int(input("Escolha uma opção: "))
+            opcao = input("Escolha uma opção: ")
+
             if opcao == '1':
                 cadastrar_operadores()
             elif opcao == '2':
@@ -189,7 +243,20 @@ def menu():
             elif opcao == '3':
                 atualizar_operadores()
             elif opcao == '4':
-                excluir_operadoes()
+                excluir_operadores()
+            elif opcao == '5':
+                cadastrar_galpão()
+            elif opcao == '6':
+                listar_galpoes()
+            elif opcao == '7':
+                atualizar_galpoes()
+            elif opcao == '8':
+                excluir_galpoes()
+            elif opcao == '9':
+                print("Saindo do sistema...")
+                break
+            else:
+                print("Opção inválida.")
 
         except KeyboardInterrupt:
             print("\n\nExecução interrompida. Fechando o sistema de forma segura...")
@@ -201,4 +268,5 @@ def menu():
     except:
         pass
 
-def menu():
+
+menu()
